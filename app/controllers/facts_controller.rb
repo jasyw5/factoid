ee94adefe485f4243fa1c69cf061e109f847a802
John Stahl <john.stahl24@gmail.com>
2018-10-02 15:45:52 +0000

    added in a form and an output for the form

--- app/controllers/facts_controller.rb
+++ app/controllers/facts_controller.rb
@@ -1,6 +1,23 @@
 class FactsController < ApplicationController
 
 def index
+  @fact = Fact.new
+  @fact_display = Fact.order("RANDOM()").first
+end
+
+def new
+  @fact = Fact.new
+end
+
+def create
+  Fact.create(fact_params)
+  redirect_to root_path
+end
+
+private
+
+def fact_params
+  params.require(:fact).permit(:fact, :author)
 end
 
 end
